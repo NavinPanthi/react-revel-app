7ce1b08e4decf97044187dac16b2709c7760f867
Navin Panthi <panthinabin341@gmail.com>
2023-09-13 17:33:01 +0545

    moreinfo component completed with more interested div

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -9,6 +9,8 @@ import { BsFillCloudSunFill } from "react-icons/bs";
 import { BiTimeFive } from "react-icons/bi";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { TbLanguageHiragana } from "react-icons/tb";
+import { AiFillLike } from "react-icons/ai";
+import {FaAngleDown} from "react-icons/fa6";
 const MoreInfo = () => {
   const p = {
     className: "text-ppurple mr-1",
@@ -22,8 +24,12 @@ const MoreInfo = () => {
       <Accordion allowZeroExpanded>
         <AccordionItem>
           <AccordionItemHeading>
-            <AccordionItemButton className="text-sm font-semibold flex">
-              More information
+            <AccordionItemButton
+              className="text-sm font-semibold flex items-center justify-between"
+              aria-controls="panel-1"
+            >
+              <p>More information</p>
+              <FaAngleDown {...p} className="text-black"/>
             </AccordionItemButton>
           </AccordionItemHeading>
           <AccordionItemPanel className=" mt-1 text-xs flex flex-col gap-y-2 mb-2">
@@ -46,6 +52,23 @@ const MoreInfo = () => {
           </AccordionItemPanel>
         </AccordionItem>
       </Accordion>
+      <div className="h-[1px] bg-slate-400"></div>
+      {/* More interested div */}
+      <div className=" my-2 flex flex-col">
+        <p className="text-sm font-semibold">
+          Click on interested to stay updated about this event.
+        </p>
+        <div className="flex flex-row items-center justify-between my-1">
+          <div className="w-1/2">
+            <AiFillLike {...p}/>
+            <p className="text-[11px] pt-1 text-pgray">
+              People who have shown interested recently.
+            </p>
+          </div>
+          <button className="text-xs text-ppurple border  border-ppurple py-1 rounded-lg px-4">Interested ?</button>
+        </div>
+      </div>
+      {/* More interested div ends here*/}
     </div>
   );
 };
